10c3539ac289f9bfc85df0e5f56c109c7a385b62
ilkckn <musa.cekcen@dci-student.org>
2024-08-26 23:18:15 +0200

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
-import { useState } from "react";
 import { Link } from "react-scroll";
 import { BsMoonStars } from "react-icons/bs";
 import { IoSunnyOutline } from "react-icons/io5";
@@ -10,6 +9,8 @@ import i18n from "../../i18n/i18n";
 
 function Navbar({ toggleTheme, currentTheme }) {
   const [languageMenuOpen, setLanguageMenuOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState("home"); // Varsayılan olarak "home" aktif
+
   const { t } = useTranslation();
 
   const toggleLanguageMenu = () => {
@@ -18,6 +19,10 @@ function Navbar({ toggleTheme, currentTheme }) {
 
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
+  };
+
+  const handleSetActive = (to) => {
+    setActiveLink(to); // Tıklanan linki aktif yap
   };
 
   return (
@@ -33,44 +38,72 @@ function Navbar({ toggleTheme, currentTheme }) {
               </div>
             )}
           </div>
-          <li>
-            <li className="themeToggleButton" onClick={toggleTheme}>
-              {currentTheme === "light" ? (
-                <BsMoonStars className="darkIcon" />
-              ) : (
-                <IoSunnyOutline className="lightIcon" />
-              )}
-            </li>
+          <li className="themeToggleButton" onClick={toggleTheme}>
+            {currentTheme === "light" ? (
+              <BsMoonStars className="darkIcon" />
+            ) : (
+              <IoSunnyOutline className="lightIcon" />
+            )}
           </li>
         </div>
         <ul>
           <li>
-            <Link className="link" to="home" smooth={true} duration={1000}>
+            <Link
+              className={`link ${activeLink === "home" ? "active" : ""}`}
+              to="home"
+              smooth={true}
+              duration={1000}
+              onSetActive={() => handleSetActive("home")}
+              onClick={() => handleSetActive("home")}
+            >
               {t("home")}
             </Link>
           </li>
           <li>
-            <Link className="link" to="about" smooth={true} duration={1000}>
+            <Link
+              className={`link ${activeLink === "about" ? "active" : ""}`}
+              to="about"
+              smooth={true}
+              duration={1000}
+              onSetActive={() => handleSetActive("about")}
+              onClick={() => handleSetActive("about")}
+            >
               {t("about.header")}
             </Link>
           </li>
           <li>
             <Link
-              className="link"
+              className={`link ${activeLink === "experience" ? "active" : ""}`}
               to="experience"
               smooth={true}
               duration={1000}
+              onSetActive={() => handleSetActive("experience")}
+              onClick={() => handleSetActive("experience")}
             >
               {t("experience.header1")}
             </Link>
           </li>
           <li>
-            <Link className="link" to="projects" smooth={true} duration={1000}>
+            <Link
+              className={`link ${activeLink === "projects" ? "active" : ""}`}
+              to="projects"
+              smooth={true}
+              duration={1000}
+              onSetActive={() => handleSetActive("projects")}
+              onClick={() => handleSetActive("projects")}
+            >
               {t("projects.header1")}
             </Link>
           </li>
           <li>
-            <Link className="link" to="contact" smooth={true} duration={1000}>
+            <Link
+              className={`link ${activeLink === "contact" ? "active" : ""}`}
+              to="contact"
+              smooth={true}
+              duration={1000}
+              onSetActive={() => handleSetActive("contact")}
+              onClick={() => handleSetActive("contact")}
+            >
               {t("contact.header1")}
             </Link>
           </li>
